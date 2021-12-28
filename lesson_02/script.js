ab68b6e36d9793d6be173c9d79c55500adc3f608
“Прокопенко <Deniss_2005@ukr.net>
2021-12-28 19:44:06 +0200

    товар новый

---
+++ lesson_02/script.js
@@ -0,0 +1,34 @@
+"use strict";
+{
+  const nameProduct = "молоко";
+  const productQuantity = 30;
+  const productCategory = "продукты";
+  const productPrice = 15;
+  console.log(
+    `Общая сумма товара из категории ${productCategory}, а именно ${nameProduct} равна ${
+      productQuantity * productPrice
+    } руб`
+  );
+}
+{
+  const nameProduct = "сметана";
+  const productQuantity = 50;
+  const productCategory = "молочные продукты";
+  const productPrice = 30;
+  console.log(
+    `Общая сумма товара из категории ${productCategory}, а именно ${nameProduct} равна ${
+      productQuantity * productPrice
+    } руб`
+  );
+}
+{
+  const nameProduct = "творого";
+  const productQuantity = 150;
+  const productCategory = "молочные продукты";
+  const productPrice = 20;
+  console.log(
+    `Общая сумма товара из категории ${productCategory}, а именно ${nameProduct} равна ${
+      productQuantity * productPrice
+    } руб`
+  );
+}
